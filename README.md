694918babb3592d02367ea8232d43de2feba81f7
henrySchoolAccount <chickenmanfy@duck.com>
2024-01-11 07:13:16 -0800

    About Page

---
+++ README.md
@@ -0,0 +1,17 @@
+[![forthebadge](https://forthebadge.com/images/badges/contains-tasty-spaghetti-code.svg)](https://forthebadge.com) [![forthebadge](https://forthebadge.com/images/badges/compatibility-club-penguin.svg)](https://forthebadge.com)
+# Heat Buddies Fictional Product
+I absolutely had nothing to do with the product name or idea.
+
+## What is this?
+This is a project created for school. The project was to design
+a website for a fictional project. I'd say I did pretty well =)
+
+## Where'd you get those images?
+Basically I copied them off the images tab when searching for them.
+I got photos from iPhoto, Clipart library, and Openclipart.
+
+## Can you tell me anything else?
+I just created this section so I could write crap but this was made mainly to learn the React
+Javascript framework. School gave me the idea, but I would've done something similar without it.
+This project is unfinished, and I will likely keep brushing it up until the end of time. If I don't,
+I forgot about it. I licensed this under the Mozilla Public License 2.0, like I do to all my projects.
---
+++ src/App.js
@@ -0,0 +1,97 @@
+import foreground from "./Images/foreground.png"
+import background from "./Images/background.png";
+import umbrella0 from "./Images/istockphoto-1034398178-640x640.jpg";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+
+function App() {
+  return (
+    <div class="website">
+      <Parallax pages={3}>
+        {/*Background*/}
+        
+        <ParallaxLayer
+          speed={0.25}
+          factor={4}
+          style={{
+            backgroundImage: `url(${background})`,
+            backgroundSize: `cover`,
+          }}
+        />
+
+        <ParallaxLayer speed={0.75}>
+          <h1
+            class="Title"
+            style={{ display: "flex", justifyContent: "center" }}
+          >
+            The future of Umbrellas.
+          </h1>
+
+          <div class="warning">
+            <p style={{ display: "flex", justifyContent: "center" }}>
+            ⓘ Please note that this project is not real.
+            </p>
+            <p style={{ display: "flex", justifyContent: "center" }}>
+              Do not try to contact me about this.
+            </p>
+          </div>
+        </ParallaxLayer>
+
+         <ParallaxLayer class="image" speed={0.1} offset={0.3}>
+          <img src="https://media.istockphoto.com/id/1034398178/video/slow-motion-of-rain-and-umbrella.jpg?s=640x640&k=20&c=ffGz-1pYPS6eH6hZfa5otc1EZ5Q3dWNPRuXaaeYeUrg="></img>
+        </ParallaxLayer>
+        <ParallaxLayer speed={0.2} offset={1}>
+          <div className="Title" style={{display: "flex",justifyContent: "center",}}>
+            <h1>Who We Are</h1>
+          </div>
+          <div class="Who">
+          <small style={{display: "flex",justifyContent: "center", fontSize: "2"}}>ⓘ please remember that this is fictional</small>
+          <p style={{display: "flex",justifyContent: "center",}}>One day, our founder,&nbsp;<a style={{display: "flex",justifyContent: "center",}} href="https://duckduckgo.com/?q=random+name">Richie Bolling</a>, was </p><p style={{display: "flex",justifyContent: "center",}}>walking home when he realized that umbrellas suck. For</p><p style={{display: "flex",justifyContent: "center",}}>many years, they have the exact same, with the same problems.</p><p style={{display: "flex",justifyContent: "center",}}>Richie put together a team to fix them.</p>
+          </div>
+        </ParallaxLayer>
+        <ParallaxLayer speed={0.2} offset={2}>
+          <h1 style={{display: "flex",justifyContent: "center",}}>How are we different?</h1>
+          <p style={{display: "flex",justifyContent: "center",}}><strike>Parallax</strike></p>
+          <small style={{display: "flex",justifyContent: "center",}}>ⓘ this product is still fake</small>
+
+          <p style={{display: "flex",justifyContent: "center",}}>Our product, Heat Buddies™, was designed with the 21<sup style={{display: "flex",justifyContent: "center",}}>st</sup> century in mind.</p><p style={{display: "flex",justifyContent: "center",}}>The handle contains a heater to keep your hand warm while holding,</p><p style={{display: "flex",justifyContent: "center",}}>hense the name. Other features include:</p>
+          <ol>
+            <li style={{display: "flex",justifyContent: "center",}}>LCD screen showing predicted weather</li>
+            <li style={{display: "flex",justifyContent: "center",}}>Thunderbolt charging+data transfer port</li>
+            <li style={{display: "flex",justifyContent: "center",}}>Companion app you can connect to with bluetooth</li>
+            <li style={{display: "flex",justifyContent: "center",}}>4 customizable buttons</li>
+            <li style={{display: "flex",justifyContent: "center",}}>Motor for automatically opening and shaking off umbrella</li>
+            <li style={{display: "flex",justifyContent: "center",}}>Raspberry Pi Zero inside</li>
+            <li style={{display: "flex",justifyContent: "center",}}>Compatible with Ultimate Doom (game purchase required)</li>
+          </ol>
+      
+          <h1 style={{display: "flex",justifyContent: "center",}}>Egg McMuffin</h1>
+        </ParallaxLayer>
+        <ParallaxLayer
+          speed={1}
+          factor={1}
+          style={{
+            backgroundImage: `url(${foreground})`,
+            backgroundSize: `cover`,
+          }}
+        />
+      </Parallax>
+    </div>
+  );
+}
+
+window.onscroll = function () {
+  myFunction();
+};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.scrollY >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+export default App;
